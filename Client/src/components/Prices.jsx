e8af0c4a64998b74efea1e9d83b8b84006a574c2
JasmeetSgaba404 <137893143+JasmeetSgaba404@users.noreply.github.com>
2024-02-01 17:57:14 +0530

    Add files via upload

---
+++ Client/src/components/Prices.jsx
@@ -0,0 +1,22 @@
+export const Prices = [
+    {
+        _id: 0,
+        name: "$0 to 50",
+        array: [0, 50],
+    },
+    {
+        _id: 1,
+        name: "$51 to 100",
+        array: [51, 100],
+    },
+    {
+        _id: 2,
+        name: "$101 to 200",
+        array: [101, 199],
+    },
+    {
+        _id: 3,
+        name: "$200 & above",
+        array: [200, 50000],
+    },
+];
